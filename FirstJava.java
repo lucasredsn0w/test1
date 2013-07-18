7c21747a59667774f24f447d6a80154dbaf434fe
lucasredsn0w <270436171@qq.com>
2013-07-18 10:35:31 +0800

    upload first file.

---
+++ FirstJava.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package firstJava;
+
+/**
+ * @author Lucas
+ *
+ */
+public class FirstJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello,world!");
+
+	}
+
+}
